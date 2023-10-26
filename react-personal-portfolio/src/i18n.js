ebb7eb55ca673db145ac6049031599ef0be8c0d3
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2023-10-26 12:49:16 +0300

    language setted to localStorage

--- react-personal-portfolio/src/i18n.js
+++ react-personal-portfolio/src/i18n.js
@@ -1,28 +1,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
-// import LanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from "i18next-browser-languagedetector";
 
-// const resources = {
-//   tr: {
-//     translation: {
-//       header: "Ben bir Frontend Geliştiricisiyim...",
-//     },
-//   },
-//   en: {
-//     translation: {
-//       header: "I am a Frontend Developer...",
-//     },
-//   },
-// };
-
-i18n
-  .use(initReactI18next)
-  .use(Backend)
-  // .use(LanguageDetector)
-  .init({
-    lng: "en",
-    //   resources,
-  });
+i18n.use(initReactI18next).use(Backend).use(LanguageDetector).init({
+  fallbackLng: "en",
+});
 
 export default i18n;
